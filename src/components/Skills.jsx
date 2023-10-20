16af16e2452de5218134f8c16f99758f10273890
charithasekara <86426242+charithasekara@users.noreply.github.com>
2023-10-20 11:03:26 +0530

    main update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,52 +1,37 @@
 import React from 'react';
 import './Skills.css';
-import ReactImg from '../assets/react.png';
-import VueImg from '../../public/vue.png';
-import FlutterImg from '../../public/flutter.png';
-import HtmlImg from '../../public/html.png';
-import CssImg from '../../public/css-3.png';
-import BootImg from '../../public/bootstrap.png';
-import AiImg from '../../public/ai.png';
-import PsImg from '../../public/ps.png';
-import XdImg from '../../public/xd.png';
-import FigmaImg from '../../public/figma.png';
+
+
 
 const Skills = () => {
+  const cardsData = [
+    { id: 1, imageUrl: 'react.png' },
+    { id: 2, imageUrl: 'vue.png' },
+    { id: 3, imageUrl: 'flutter.png' },
+    { id: 4, imageUrl: 'html.png' },
+    { id: 5, imageUrl: 'css-3.png' },
+    { id: 6, imageUrl: 'bootstrap.png' },
+    { id: 7, imageUrl: 'ai.png' },
+    { id: 8, imageUrl: 'ps.png' },
+    { id: 9, imageUrl: 'xd.png' },
+    { id: 10, imageUrl: 'figma.png' },
+  ];
+
   return (
     <div className="card-grid">
       <div className="card-set">
-        <div className="card">
-          <img src={ReactImg} alt="Card 1" />
-        </div>
-        <div className="card">
-          <img src={VueImg} alt="Card 2" />
-        </div>
-        <div className="card">
-          <img src={FlutterImg} alt="Card 3" />
-        </div>
-        <div className="card">
-          <img src={HtmlImg} alt="Card 4" />
-        </div>
-        <div className="card">
-          <img src={CssImg} alt="Card 5" />
-        </div>
+        {cardsData.slice(0, 5).map((card) => (
+          <div key={card.id} className="card">
+            <img src={card.imageUrl} alt={`Card ${card.id}`} />
+          </div>
+        ))}
       </div>
       <div className="card-set">
-        <div className="card">
-          <img src={BootImg} alt="Card 6" />
-        </div>
-        <div className="card">
-          <img src={AiImg} alt="Card 7" />
-        </div>
-        <div className="card">
-          <img src={PsImg} alt="Card 8" />
-        </div>
-        <div className="card">
-          <img src={XdImg} alt="Card 9" />
-        </div>
-        <div className="card">
-          <img src={FigmaImg} alt="Card 10" />
-        </div>
+        {cardsData.slice(5, 10).map((card) => (
+          <div key={card.id} className="card">
+            <img src={card.imageUrl} alt={`Card ${card.id}`} />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import{FaEnvelopeOpen} from 'react-icons/fa';
 import{FaPhoneSquareAlt} from 'react-icons/fa';
+import{FaFacebookF} from 'react-icons/fa';
+import{FaTwitter} from 'react-icons/fa';
+import{FaYoutube} from 'react-icons/fa';
 import{ImDribbble} from 'react-icons/im';
 import{PiFigmaLogoThin} from 'react-icons/pi';
 import {FaGithub} from 'react-icons/fa';
@@ -8,7 +11,6 @@ import {TbBrandFiverr} from 'react-icons/tb';
 
 import{FiSend} from 'react-icons/fi';
 import'./contact.css';
-import { ContactUs } from '../../components/ContactUs';
 
 const Contact = () => {
   return (
@@ -65,7 +67,43 @@ const Contact = () => {
         </div>
       </div>
 
-    <ContactUs/>
+      <form action="" className="contact__form">
+
+        <div className="form__input-group">
+          <div className="form__input-div">
+            <input 
+            type="text" 
+            placeholder='Your Name'
+            className="form__control"/>
+          </div>
+
+          <div className="form__input-div">
+            <input 
+            type="email" 
+            placeholder='Your Email'
+             className="form__control" />
+          </div>
+
+          <div className="form__input-div">
+            <input 
+            type="text" 
+            placeholder='Your Subject'
+             className="form__control" />
+          </div>
+
+        </div>
+
+        <div className="form__input-div">
+          <textarea placeholder='Your Message' className="form__control textarea"></textarea>
+        </div>
+
+        <button className="button">
+          Send Message
+          <span className="button__icon contact__button-icon">
+            <FiSend/>
+          </span>
+        </button>
+      </form>
     </div>
     </section>
   )
